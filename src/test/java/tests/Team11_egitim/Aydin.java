07649323dc192de75c9f992fe987a0248ddb98bf
Aydin Cellek <aydincellek86@gmail.com>
2022-06-02 17:10:46 +0200

    yeni mesaj

---
+++ src/test/java/tests/Team11_egitim/Aydin.java
@@ -0,0 +1,11 @@
+package tests.Team11_egitim;
+
+import org.testng.annotations.Test;
+
+public class Aydin {
+
+    @Test
+    public void testaydin() {
+        System.out.println("asdfgh");
+    }
+}
