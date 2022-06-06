3a80fef4f17e05068bb6a5068c7e1ff115b40779
Adem Polat <polatadem.2541@gmail.com>
2022-06-06 14:31:25 +0200

    06062022

---
+++ src/test/java/tests/Team11_egitim/AdemIstHere.java
@@ -0,0 +1,10 @@
+package tests.Team11_egitim;
+
+import org.testng.annotations.Test;
+
+public class AdemIstHere {
+    @Test
+    public void testAdem() {
+        System.out.println("Hello World");
+    }
+}
